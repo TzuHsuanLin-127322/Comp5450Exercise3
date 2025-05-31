38f1af631172b2d900c3782daa0187fdd46658d5
Tzu-Hsuan Lin (1273233) <tlin3@lakeheadu.ca>
2025-05-30 22:37:02 -0400

    Display game over

--- lib/main.dart
+++ lib/main.dart
@@ -32,28 +32,30 @@ class GamePlay extends StatefulWidget {
   @override
   State<GamePlay> createState() => _GamePlayState();
 }
-
+const int MAX_LIVES = 3;
+const double BALL_SIZE = 30.0;
 class _GamePlayState extends State<GamePlay>
     with SingleTickerProviderStateMixin {
-  bool _isPaused = false;
 
+  late Ticker ticker;
+
+  bool _isPaused = false;
   int _score = 0;
+  int _lives = MAX_LIVES;
+  final List<Ball> _balls = [];
+  double _nextBallSpawnTime = 0.0;
+  Duration? _lastTickTime;
+
   double bucketX = 0.0;
   double bucketWidth = 100.0;
   double bucketHeight = 50.0;
 
-  List<Ball> _balls = [];
-  double _nextBallSpawnTime = 0.0;
-
-  late Ticker ticker;
-  Duration? _lastTickTime;
   final Random _rng = Random();
 
   BoxConstraints _gameAreaSize = BoxConstraints();
 
   @override
   void initState() {
-    print('initState');
     // TODO: implement initState
     super.initState();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
@@ -75,15 +77,18 @@ class _GamePlayState extends State<GamePlay>
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('Score: $_score', style: TextStyle(fontSize: 20)),
-                Row(
+                Expanded(child: _renderLives()),
+                Expanded(child: Center(child: Text('$_score', style: TextStyle(fontSize: 20)))),
+                Expanded(child:  Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     IconButton(
-                      onPressed: () => togglePause(),
+                      onPressed: () => _togglePause(),
                       icon: Icon(_isPaused ? Icons.play_arrow : Icons.pause),
                     ),
                   ],
-                ),
+                ),)
+                
               ],
             ),
           ),
@@ -117,7 +122,6 @@ class _GamePlayState extends State<GamePlay>
   }
 
   void _onTick(Duration duration) {
-    print('ticker._onTick');
     double deltaTime = 0;
     if (_lastTickTime != null) {
       deltaTime = (duration.inMilliseconds - _lastTickTime!.inMilliseconds)
@@ -131,7 +135,6 @@ class _GamePlayState extends State<GamePlay>
   }
 
   void _updateObjectPosition() {
-    double screenHeight = _getScreenSize().height;
     double bucketTop = _gameAreaSize.maxHeight * 0.9;
     double bucketLeft = bucketX;
     double bucketRight = bucketX + bucketWidth;
@@ -149,19 +152,20 @@ class _GamePlayState extends State<GamePlay>
       // Bucket Collision
       if (ballBottomCrossBucketTop && ballTouchBucketSide) {
         _score++;
+        _lives = MAX_LIVES;
         _balls.remove(ball);
       }
       // Ball out of screen
-      if (ball.position[1] > screenHeight) {
+      if (ball.position[1] > _gameAreaSize.maxHeight) {
         _balls.remove(ball);
+        _lives--;
+        _checkGameOver();
       }
     }
   }
 
   void _updateSpawnTimer(double deltaTime) {
-    print(deltaTime);
     _nextBallSpawnTime -= deltaTime;
-    print('nextBallSpawnTime: $_nextBallSpawnTime');
     if (_nextBallSpawnTime <= 0.0) {
       _spawnBall();
       _nextBallSpawnTime =
@@ -170,11 +174,16 @@ class _GamePlayState extends State<GamePlay>
   }
 
   void _spawnBall() {
-    double ballWidth = 10;
-    double x = _rng.nextDouble() * (_getScreenSize().width - ballWidth);
+    double x = _rng.nextDouble() * (_getScreenSize().width - BALL_SIZE);
     double speed = 1 + (_rng.nextBool() ? 1 : -1) * (_rng.nextDouble() / 2);
     _balls.add(
-      Ball(Colors.black, [x, 0.05 * _getScreenSize().height], [0, speed]),
+      Ball(_randomColor(), [x, 0.05 * _getScreenSize().height], [0, speed], [30, 30]),
+    );
+  }
+
+  Widget _renderLives() {
+    return Row(
+      children: _lives > 0 ? List.generate(_lives, (index) => Icon(Icons.favorite)) : [],
     );
   }
 
@@ -210,6 +219,9 @@ class _GamePlayState extends State<GamePlay>
 
   void _onHorizontalDragUpdate(DragUpdateDetails details) {
     setState(() {
+      if (_isPaused) {
+        return;
+      }
       bucketX = clampDouble(
         bucketX + details.delta.dx,
         0,
@@ -218,18 +230,54 @@ class _GamePlayState extends State<GamePlay>
     });
   }
 
-  void togglePause() {
+  void _togglePause() {
     setState(() {
       _isPaused = !_isPaused;
       if (_isPaused) {
-        print('pause');
         ticker.stop();
         _lastTickTime = null;
       } else {
-        print('resume');
         ticker.start();
       }
     });
+  }
+
+  void _checkGameOver() {
+    if (_lives > 0) {
+      return;
+    }
+    _isPaused = true;
+    ticker.stop();
+    _lastTickTime = null;
+    showDialog(context: context, builder:(context) => AlertDialog(
+      title: Text('Game Over'),
+      content: Text('Your score is $_score'),
+      actions: [
+        TextButton(
+          child: Text('Play Again'),
+          onPressed: () {
+            _resetGame();
+            Navigator.of(context).pop();
+          }
+        )
+      ]
+    ));
+  }
+
+  void _resetGame() {
+    setState(() {
+      _score = 0;
+      _lives = MAX_LIVES;
+      _balls.clear();
+      _nextBallSpawnTime = 0.0;
+      _lastTickTime = null;
+      _isPaused = false;
+      ticker.start();
+    });
+  }
+
+  Color _randomColor() {
+    return Color.fromARGB(255, _rng.nextInt(256), _rng.nextInt(256), _rng.nextInt(256));
   }
 
   Size _getScreenSize() => MediaQuery.of(context).size;
@@ -240,8 +288,8 @@ class Ball {
   List<double> previousPosition = [0.0, 0.0];
   List<double> position = [0.0, 0.0];
   List<double> velocity = [0.0, 0.0];
-  List<double> size = [10.0, 10.0];
-  Ball(this.color, this.position, this.velocity);
+  List<double> size = [BALL_SIZE, BALL_SIZE];
+  Ball(this.color, this.position, this.velocity, this.size);
 
   void updatePosition() {
     previousPosition[0] = position[0];
